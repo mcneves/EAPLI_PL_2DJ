cbc30c910efc5f573eff111293e9361a01e1ca5a
Tiago Vieira <tiago-wtf@hotmail.com>
2013-04-03 17:35:27 +0100

    Criadas classes DefPaymentMeansController Cash PaymentMeans PaymentMeansRepository DefPaymentMeansUI TiagoVieira1110578 PauloCosta1101413 fixes #12

--- eapli.ExpenseManager/src/Presentation/MainMenu.java
+++ eapli.ExpenseManager/src/Presentation/MainMenu.java
@@ -12,39 +12,43 @@ import eapli.util.Console;
  */
 public class MainMenu {
 
-      public MainMenu() {
-      }
+    public MainMenu() {
+    }
 
-      public void mainLoop() {
-            int op;
-            do {
-                  op = menu();
-                  switch (op) {
-                        case 0:
-                              System.out.println("bye bye ...");
-                              break;
-                        case 1:
-                              ExpenseRegisterUI ui = new ExpenseRegisterUI();
-                              ui.run();
-                              break;
-                        case 2:
-                              ShowExpensesUI showExpensesUI = new ShowExpensesUI();
-                              showExpensesUI.loop();
-                              break;
-                  }
-            } while (op != 0);
+    public void mainLoop() {
+        int op;
+        do {
+            op = menu();
+            switch (op) {
+                case 0:
+                    System.out.println("bye bye ...");
+                    break;
+                case 1:
+                    ExpenseRegisterUI ui = new ExpenseRegisterUI();
+                    ui.run();
+                    break;
+                case 2:
+                    ShowExpensesUI showExpensesUI = new ShowExpensesUI();
+                    showExpensesUI.loop();
+                    break;
+                case 3:
+                    DefPaymentMeansUI defPaymentMeansUI = new DefPaymentMeansUI();
+                    defPaymentMeansUI.loop();
+            }
+        } while (op != 0);
 
-      }
+    }
 
-      private static int menu() {
-            System.out.println("===================");
-            System.out.println("  EXPENSE MANAGER  ");
-            System.out.println("===================\n");
-            System.out.println("1. Register an expense");
-            System.out.println("2. Show expenses");
-            System.out.println("0. Exit\n\n");
+    private static int menu() {
+        System.out.println("===================");
+        System.out.println("  EXPENSE MANAGER  ");
+        System.out.println("===================\n");
+        System.out.println("1. Register an expense");
+        System.out.println("2. Show expenses");
+        System.out.println("3. Define payment means");
+        System.out.println("0. Exit\n\n");
 
-            int option = Console.readInteger("Please choose an option");
-            return option;
-      }
+        int option = Console.readInteger("Please choose an option");
+        return option;
+    }
 }
