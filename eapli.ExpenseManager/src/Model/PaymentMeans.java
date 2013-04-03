ae120e11eefdc9f58074297a6b73ebf6e34afe15
PauloCosta2DJ <byic@yahoo.com>
2013-04-03 15:13:30 +0100

    Criação de SuperClasse PaymentMeans

---
+++ eapli.ExpenseManager/src/Model/PaymentMeans.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Paulo Costa
+ */
+public class PaymentMeans {
+    
+    
+    
+}
