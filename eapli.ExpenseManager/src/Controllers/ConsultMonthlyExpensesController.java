395be9e9633cbab6a8ff75c59baf5aaede48566a
1121096 <1121096@isep.ipp.pt>
2013-04-17 18:13:04 +0100

    Finalizada a tarefa Consulta de gastos mensais agrupados por tipo de despesa

---
+++ eapli.ExpenseManager/src/Controllers/ConsultMonthlyExpensesController.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+import Model.Expense;
+import Model.ExpenseRecord;
+import Model.ExpenseType;
+import Persistence.ExpenseRepository;
+import java.util.List;
+import Model.MonthExpensesByType;
+import Persistence.ExpenseTypeRepository;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author i121096
+ */
+public class ConsultMonthlyExpensesController {
+    
+    public ConsultMonthlyExpensesController(){
+    }
+    
+  
+    public List<MonthExpensesByType> getMonthExpensesByType(int month){
+          
+        ShowExpensesController controller = new ShowExpensesController();
+        List<Expense> listaTotal = controller.getAllExpenses();
+        ExpenseRecord recorder = new ExpenseRecord(listaTotal);
+        ExpenseTypeRepository repository = new ExpenseTypeRepository();
+        
+        
+        List<Expense> monthExpenses = recorder.getListOfExpensesMonth(month);
+        List<ExpenseType> expensesTypes = repository.ListAllTypes();
+        
+        List<MonthExpensesByType> listMonthExpensesByType = new ArrayList<MonthExpensesByType>();
+        
+        for(int i = 0; i < expensesTypes.size(); i++){
+         listMonthExpensesByType.add(new MonthExpensesByType(expensesTypes.get(i).getDescription()));
+        }
+        
+        for(int i = 0; i < monthExpenses.size(); i++){
+            for(int j = 0; j < listMonthExpensesByType.size(); j++){
+             if(monthExpenses.get(i).getExpenseType().getDescription().equals(listMonthExpensesByType.get(j).getExpenseType())){
+                 listMonthExpensesByType.get(j).addExpense(monthExpenses.get(i));
+             }
+            }
+        }
+        
+        
+        
+        return listMonthExpensesByType;
+    }
+}
