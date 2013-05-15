a2dd09a30e35da6c320c2e6db2cb0968f1605856
VitorHugo <VitorHugo@VitorHugo>
2013-05-15 17:20:49 +0100

    Expense Persistence done

--- eapli.ExpenseManager/src/Controllers/ExpenseRegisterController.java
+++ eapli.ExpenseManager/src/Controllers/ExpenseRegisterController.java
@@ -7,9 +7,11 @@ package Controllers;
 import Model.Expense;
 import Model.ExpenseType;
 import Model.PaymentMeans;
+import Persistence.IExpenseRepository;
 import Persistence.PersistenceRegistry;
 import java.math.BigDecimal;
 import java.util.Date;
+import Persistence.PersistenceFactory;
 
 /**
  *
@@ -22,6 +24,7 @@ public class ExpenseRegisterController extends BaseController{
     
     public void registerExpense(String desc, Date date, BigDecimal amount, ExpenseType exptype, String comment, PaymentMeans pay) {
             Expense expense = new Expense(desc, date, amount, exptype, comment, pay);
+            IExpenseRepository repo = PersistenceFactory.getInstance().buildRepositoryFactory().getExpenseRepository();
             PersistenceRegistry.getInstance().expenseRepository().saveExpense(expense);
     }
 }
