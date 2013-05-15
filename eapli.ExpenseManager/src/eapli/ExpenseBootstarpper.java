c95be393d070293f3a7bf66ee06e8e855a8cecb6
VitorHugo <VitorHugo@VitorHugo>
2013-05-15 17:52:44 +0100

    Expenses bootstrapper remake

--- eapli.ExpenseManager/src/eapli/ExpenseBootstarpper.java
+++ eapli.ExpenseManager/src/eapli/ExpenseBootstarpper.java
@@ -13,6 +13,7 @@ import Persistence.IExpenseRepository;
 import Persistence.IExpenseTypeRepository;
 import Persistence.IPaymentMeansRepository;
 import Persistence.PaymentMeansRepository;
+import Persistence.PersistenceFactory;
 import java.math.BigDecimal;
 
 /**
@@ -22,9 +23,11 @@ import java.math.BigDecimal;
 public class ExpenseBootstarpper {
 
     static {
-        IExpenseRepository repo = new ExpenseRepository();
-        IExpenseTypeRepository trepo = new ExpenseTypeRepository();
-        IPaymentMeansRepository prepo = new PaymentMeansRepository();
+        IExpenseRepository repo = PersistenceFactory.getInstance().buildRepositoryFactory().getExpenseRepository();
+        IExpenseTypeRepository trepo = PersistenceFactory.getInstance().buildRepositoryFactory().getExpenseTypeRepository();
+        IPaymentMeansRepository prepo = PersistenceFactory.getInstance().buildRepositoryFactory().getPaymentMeansRepository();
+       
+       
 
        /* ExpenseType vec[] = new ExpenseType[4];
         PaymentMeans arr[] = new PaymentMeans[7];
@@ -36,6 +39,7 @@ public class ExpenseBootstarpper {
             arr[i] = prepo.getAllMeans().get(i);
         }*/
 
+   
         repo.saveExpense(new Expense("Autocarro", 2013, 3, 4, new BigDecimal(2.4), trepo.ListAllTypes().get(2), "", prepo.getAllMeans().get(1)));
         repo.saveExpense(new Expense("Almoço", 2013, 3, 5, new BigDecimal(12.4), trepo.ListAllTypes().get(1), "", prepo.getAllMeans().get(2)));
         repo.saveExpense(new Expense("Sapatilhas", 2013, 3, 4, new BigDecimal(123.4), trepo.ListAllTypes().get(0), "", prepo.getAllMeans().get(4)));
