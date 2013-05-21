323c13146e0c282decf3fedc09713748cf8ae594
AndrePinto-1090534 <1090534@isep.ipp.pt>
2013-05-21 21:55:25 +0100

    Criado class AccountServices com o metodo getBalance();

---
+++ eapli.ExpenseManager/src/Model/AccountServices.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import Persistence.PersistenceFactory;
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ *
+ * @author André Pinto
+ */
+public class AccountServices {
+
+    public AccountServices() {
+    }
+
+    public BigDecimal getBalance() {
+        BigDecimal amount = new BigDecimal("0"), expenseCount = new BigDecimal("0"), incomeCount = new BigDecimal("0");
+        List<Expense> listExp = PersistenceFactory.getInstance().buildRepositoryFactory().getExpenseRepository().getAllExpenses();
+        List<Income> listIncome = PersistenceFactory.getInstance().buildRepositoryFactory().getIncomeRepository().getAllIncome();
+
+        for (Expense exp : listExp) {
+            expenseCount = expenseCount.add(exp.getAmount());
+        }
+        for (Income inc : listIncome) {
+            incomeCount = incomeCount.add(inc.getAmount());
+        }
+
+        amount = round(incomeCount,2,true).subtract(round(expenseCount,2,true));
+
+        //prints testes
+        //System.out.println("Soma Expenses: " + expenseCount);
+        //System.out.println("Soma Incomes: " + incomeCount);
+        //System.out.println("Saldo: " + amount);
+
+        return amount;
+    }
+
+    //metodo para arredondar BigDecimal
+    private static BigDecimal round(BigDecimal d, int scale, boolean roundUp) {
+        int mode = (roundUp) ? BigDecimal.ROUND_UP : BigDecimal.ROUND_DOWN;
+        return d.setScale(scale, mode);
+    }
+}
