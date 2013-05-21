cabd9d8772090c2e51113745e36bf91e6c28c554
AndrePinto-1090534 <1090534@isep.ipp.pt>
2013-05-21 14:27:21 +0100

    Desapareceu construtor vazio! reposiçao do construtor!

--- eapli.ExpenseManager/src/Model/IncomeType.java
+++ eapli.ExpenseManager/src/Model/IncomeType.java
@@ -6,6 +6,10 @@ import javax.persistence.Entity;
 @Entity
 public class IncomeType extends TransactionType{
 
+    public IncomeType() {
+    }
+
+    
     public IncomeType(String description) {
         super(description);
     }
