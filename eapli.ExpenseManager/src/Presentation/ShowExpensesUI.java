1d1abda748f7892a0331f031679db85964813443
Tiago Vieira <tiago-wtf@hotmail.com>
2013-05-22 08:08:44 +0100

    Corrigido o ShowLastExpense TiagoVieira1110578

--- eapli.ExpenseManager/src/Presentation/ShowExpensesUI.java
+++ eapli.ExpenseManager/src/Presentation/ShowExpensesUI.java
@@ -4,6 +4,7 @@
  */
 package Presentation;
 
+import Controllers.BaseController;
 import Controllers.ShowExpensesController;
 import Model.Expense;
 import eapli.util.Console;
@@ -11,53 +12,61 @@ import java.util.List;
 
 /**
  *
- * @author 
+ * @author
  */
-public class ShowExpensesUI {
-      public ShowExpensesUI() {
-      }
+public class ShowExpensesUI extends BaseUI {
 
-      public void loop() {
-            int op;
-            Expense expense=null;
-            ShowExpensesController controller = new ShowExpensesController();
-            do {
-                  op = menu();              
-                  switch (op) {
-                        case 0:
-                              System.out.println(" End show expenses");
-                              break;
-                        case 1:
-                              List<Expense> list= controller.getAllExpenses();
-                              displayList(list);
-                              break;
-                        case 2:
-                                 expense = controller.getLastExpense();
-                              System.out.println("Last Expense"+expense);
-                              break;
-                        default:
-                              System.out.println("Wrong option. Please repeat");
-                              break;
-                  }
-            } while (op != 0);
-      }
+    public ShowExpensesUI() {
+    }
+    ShowExpensesController controller = new ShowExpensesController();
 
-      private int menu() {
-            System.out.println("* * *  SHOW EXPENSES  * * *\n");
-            System.out.println("1. All expenses");
-            System.out.println("2. Last expense");
-            System.out.println("0. End show expenses\n\n");
-            int op = Console.readInteger("Choose an option");
-            return op;
-      }
+    public BaseController controller() {
+        return controller;
+    }
 
- 
-      private void displayList(List<Expense> list) {
-            int i=0;
-            System.out.println("List of Expenses\n");
-            for (Expense exp : list) {
-                  i=i+1;
-                  System.out.println("Expense "+i+"\n" + exp);
+    public void run() {
+        int op;
+        Expense exp;
+        do {
+            op = menu();
+            switch (op) {
+                case 0:
+                    System.out.println(" End show expenses");
+                    break;
+                case 1:
+                    List<Expense> list = controller.getAllExpenses();
+                    displayList(list);
+                    break;
+                case 2:
+                    exp = controller.getLastExpense();
+                    System.out.println("Last Expense" + exp);
+                    break;
+                default:
+                    System.out.println("Wrong option. Please repeat");
+                    break;
             }
-      }
+        } while (op != 0);
+    }
+
+    private int menu() {
+        System.out.println("1. All expenses");
+        System.out.println("2. Last expense");
+        System.out.println("0. End show expenses\n\n");
+        int op = Console.readInteger("Choose an option");
+        return op;
+    }
+
+    private void displayList(List<Expense> list) {
+        int i = 0;
+        System.out.println("List of Expenses\n");
+        for (Expense exp : list) {
+            i = i + 1;
+            System.out.println("Expense " + i + "\n" + exp);
+        }
+    }
+
+    @Override
+    public void header() {
+        System.out.println("* * *  SHOW EXPENSES  * * *\n");
+    }
 }
